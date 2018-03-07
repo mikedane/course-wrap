1c50027328e196bf4568765288f30321f8620fb9
mikedane <mikedane94@gmail.com>
2018-03-06 23:10:11 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpk Bbel Se';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+  );
+
+module.hot.accept();
